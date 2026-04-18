d5717ce8cdbbd14eae08a8c5542b810c33e6f22e
SageDragoon <sagedragoon79@gmail.com>
2026-04-17 19:33:36 -0500

    Update AssemblyInfo file version to match mod version (1.1.3)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,32 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ResourceRelocationButton")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Forageable Transplantation")]
+[assembly: AssemblyDescription("Relocate any forageable type in Farthest Frontier")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ResourceRelocationButton")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCompany("SageDragoon")]
+[assembly: AssemblyProduct("Forageable Transplantation")]
+[assembly: AssemblyCopyright("Copyright © SageDragoon 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0bcbec72-1a3b-4836-b4f4-d0a46aff3e2b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
